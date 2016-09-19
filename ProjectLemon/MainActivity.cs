ce6d84ce0d5ddda3efa4df2cfdfd4ba37185610b
HernanMedH <alexyz656@gmail.com>
2016-09-19 15:33:17 -0700

    Dibujo de ruta y decriptación de codigo de ruta

--- ProjectLemon/MainActivity.cs
+++ ProjectLemon/MainActivity.cs
@@ -9,6 +9,7 @@ using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
 using System.Net;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ProjectLemon
 {
@@ -53,8 +54,14 @@ namespace ProjectLemon
             var client = new WebClient();
             var content = client.DownloadString(url);
             dynamic route = JObject.Parse(content);
-            //string something = route.name;
-
+            List<Location> locList = DecodePolylinePoints("affeEjogiU]fAc@rAgBlFq@lBEV?LJf@`@hBlAzG}@n@OPa@FuANb@jK?NkAH{Gf@YtEUdCOZYRm@Z_@f@Qf@Y|BEb@bEr@lF`ATAVB~AXjBr@`@RHQ`CcEt@iAl@i@j@w@rBqDjDyF~DaGpBsCbAaBvCqEnEyG~A}B|BoDzBkDfA}BtBgFt@iBd@qA^uAPeAXoDd@cGLkCCo@O}@iAsDSk@y@eCmAkDUy@U}A]wD_@_FIw@a@wBMe@[_Am@aAk@{@iAgAkAw@m@YqDeAuAe@q@[iBwAu@_Ac@u@a@{@k@{AWcASaAUiBO{B@kCLqBN{AVkA|@}ChBmGx@uCTwA@e@?w@Eq@Kw@Om@Uk@Sc@gA_BqFqI}@eBuAyBk@}@wAoBgHiKiIwL_A{A_AmBmAgC_CyFaAiBy@kAm@k@aBqAgC_BoE{CeCiBiB{AyBkB{KwJwBkBs@w@_AsAm@cAgAwD_AkDeB_Gw@aD[oA]uB]sAYgBg@uCc@gBm@{A]m@q@u@g@_@oEuDeDyC{CsCqFcEs@w@}@q@mFeDmB{Aa@YsA_A}EwDiNgK{AoAqHqFgBiAsF{DwAo@{@QkAIo@Aq@B{Dt@{Ez@uEt@uCd@cCh@y@Pm@iAmAyBi@o@MSI]EYMQBs@z@kXXoIHeANmBTyA\\iAb@}@lAaB~@eAhB{BfA}A`@y@t@yBXaBZaCtA{NRqCJwBBgBCiBGkAIu@k@_CuEuOiGkQgA}Du@qDo@yDqA{LCiANwCFeB?gAGwAMuADWAYGq@Mk@eBqFUw@Km@AU?OFYDIHIx@a@\\MzB{@nC{@vDsA`EyA`Dw@vHkArB[bC]^Fv@?vCSPC?\\NlBFp@AHg@d@KP@ZJv@");
+            PolylineOptions pOptions = new PolylineOptions();
+            foreach (Location l in locList)
+            {
+                pOptions.Add(new LatLng(l.Latitude, l.Longitude));
+            }
+            pOptions.InvokeColor(-65536);
+            Polyline poly = map.AddPolyline(pOptions);
         }
 
         private void BtnSatellite_Click(object sender, EventArgs e)
@@ -94,7 +101,7 @@ namespace ProjectLemon
             options = new MarkerOptions().SetPosition(myLatLng).SetTitle("Das Marker").SetSnippet("das snippet").Draggable(true);
             map.AddMarker(options);
             map.MarkerDragEnd += Map_MarkerDragEnd;
-            map.TrafficEnabled = true;
+            //map.TrafficEnabled = true;
         }
 
         private void Map_MarkerDragEnd(object sender, GoogleMap.MarkerDragEndEventArgs e)
@@ -123,6 +130,64 @@ namespace ProjectLemon
         public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
         {
             //throw new NotImplementedException();
+        }
+        private List<Location> DecodePolylinePoints(string encodedPoints)
+        {
+            if (encodedPoints == null || encodedPoints == "") return null;
+            List<Location> poly = new List<Location>();
+            char[] polylinechars = encodedPoints.ToCharArray();
+            int index = 0;
+
+            int currentLat = 0;
+            int currentLng = 0;
+            int next5bits;
+            int sum;
+            int shifter;
+
+            try
+            {
+                while (index < polylinechars.Length)
+                {
+                    // calculate next latitude
+                    sum = 0;
+                    shifter = 0;
+                    do
+                    {
+                        next5bits = (int)polylinechars[index++] - 63;
+                        sum |= (next5bits & 31) << shifter;
+                        shifter += 5;
+                    } while (next5bits >= 32 && index < polylinechars.Length);
+
+                    if (index >= polylinechars.Length)
+                        break;
+
+                    currentLat += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+
+                    //calculate next longitude
+                    sum = 0;
+                    shifter = 0;
+                    do
+                    {
+                        next5bits = (int)polylinechars[index++] - 63;
+                        sum |= (next5bits & 31) << shifter;
+                        shifter += 5;
+                    } while (next5bits >= 32 && index < polylinechars.Length);
+
+                    if (index >= polylinechars.Length && next5bits >= 32)
+                        break;
+
+                    currentLng += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+                    Location p = new Location("");
+                    p.Latitude = Convert.ToDouble(currentLat) / 100000.0;
+                    p.Longitude = Convert.ToDouble(currentLng) / 100000.0;
+                    poly.Add(p);
+                }
+            }
+            catch (Exception ex)
+            {
+                // logo it
+            }
+            return poly;
         }
     }
 
